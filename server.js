bf085d3d3fb5a1b35e778cdb2dafc18ce444c548
Francis Roy Balasabas <misterpagz@MisterpagZ.local>
2021-01-16 22:42:44 -0500

    add server js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongojs = require("mongojs");
 const logger = require("morgan");
 
-const databaseUrl = "warmup";
-const collections = ["books"];
-const db = mongojs(databaseUrl, collections);
+// const databaseUrl = "warmup";
+// const collections = ["excercises"];
+// const db = mongojs(databaseUrl, collections);
+
+const PORT = process.env.PORT || 3000; 
 
 const app = express();
 
@@ -13,90 +15,102 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
+  { useNewUrlParser: true, useFindAndModify: false });
 
-app.post("/submit", ({ body }, res) => {
-  const book = body;
+app.use(require("./routes/htmlroutes.js"))
+app.use(require("./routes/api-routes.js"))
 
-  book.read = false;
-
-  db.books.save(book, (error, saved) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(saved);
-    }
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
   });
-});
 
-app.get("/read", (req, res) => {
-  db.books.find({ read: true }, (error, found) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(found);
-    }
-  });
-});
 
-app.get("/unread", (req, res) => {
-  db.books.find({ read: false }, (error, found) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(found);
-    }
-  });
-});
 
-app.put("/markread/:id", ({ params }, res) => {
-  db.books.update(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      $set: {
-        read: true
-      }
-    },
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
 
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
+// app.post("/addExcercise", ({ body }, res) => {
+//   const excercise = body;
 
-app.put("/markunread/:id", ({ params }, res) => {
-  db.books.update(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      $set: {
-        read: false
-      }
-    },
+//   excercise.read = false;
 
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
+//   db.excercises.save(excercise, (error, saved) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.send(saved);
+//     }
+//   });
+// });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000!");
-});
+// app.get("/read", (_req, res) => {
+//   db.excercises.find({ read: true }, (error, found) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+// app.get("/unread", (_req, res) => {
+//   db.excercises.find({ read: false }, (error, found) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+// app.put("/markread/:id", ({ params }, res) => {
+//   db.excercises.update(
+//     {
+//       _id: mongojs.ObjectId(params.id)
+//     },
+//     {
+//       $set: {
+//         read: true
+//       }
+//     },
+
+//     (error, edited) => {
+//       if (error) {
+//         console.log(error);
+//         res.send(error);
+//       } else {
+//         console.log(edited);
+//         res.send(edited);
+//       }
+//     }
+//   );
+// });
+
+// app.put("/markunread/:id", ({ params }, res) => {
+//   db.excercises.update(
+//     {
+//       _id: mongojs.ObjectId(params.id)
+//     },
+//     {
+//       $set: {
+//         read: false
+//       }
+//     },
+
+//     (error, edited) => {
+//       if (error) {
+//         console.log(error);
+//         res.send(error);
+//       } else {
+//         console.log(edited);
+//         res.send(edited);
+//       }
+//     }
+//   );
+// });
+
+// app.listen(3000, () => {
+//   console.log("App running on port 3000!");
+// });
