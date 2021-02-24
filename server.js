ded57b2b694b34f8a19ac6c582d9592e28764b84
Francis Roy Balasabas <misterpagz@MisterpagZ.local>
2021-02-24 00:27:22 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
-  { useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false });
+  { useNewUrlParser: true, useFindAndModify: false });
 
 app.use(require("./routes/htmlroutes.js"))
 app.use(require("./routes/apiroutes.js"))
